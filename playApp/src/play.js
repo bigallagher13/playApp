648ed6f08af7ba93cc2c062a3171a2e5c1e5ad7e
Brian Gallagher <54048833+bigallagher13@users.noreply.github.com>
2020-06-16 12:38:42 -0700

    Add files via upload

--- playApp/src/play.js
+++ playApp/src/play.js
@@ -1,7 +1,11 @@
 document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById('generate').addEventListener('click', hideButton);
 
-//let temperature = document.getElementById('temp');
-//let summary = document.getElementById('description');
+
+function hideButton() {
+  document.getElementById('generate').style.display = 'block';
+  this.style.display = 'none'
+};
 
 function performAction(e) {
   getLocation();
@@ -14,7 +18,6 @@ const getLocation = () => {
     navigator.geolocation.getCurrentPosition(position => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-      console.log(lat,lon);
 
       const proxy = 'https://cors-anywhere.herokuapp.com/';
       const api = `${proxy}https://api.darksky.net/forecast/4160f64f26a16b919b1fd34d277b676c/${lat},${lon}`;
@@ -26,10 +29,21 @@ const getLocation = () => {
           console.log(data);
           const temperature = data.currently.temperature;
           const summary = data.currently.summary;
-          temp.textContent = temperature.toFixed(0)+'°'+'F';
-          description.textContent = summary;
+          const icon = data.currently.icon;
+          temp.textContent = 'It is: '+temperature.toFixed(0)+'°'+'F';
+          description.textContent = 'Conditions: '+summary;
+          //invoke icon function
+          setIcon(icon, document.querySelector('#icon'));
+
         });
       });
+
+      function setIcon(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+      };
 
   } else {
     console.log('geolocation not available');
